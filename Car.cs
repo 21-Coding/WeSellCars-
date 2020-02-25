8a25aa850ceb4273809768dd39ea1ef9321a1333
Student <example@example.com>
2020-02-25 09:01:22 -0800

    add functionality to only show cars that are under a certain mileage and price

--- Car.cs
+++ Car.cs
@@ -7,12 +7,19 @@ public class Car
   public string MakeModel;
   public int Price;
   public int Miles;
-  public Car(string makeModel, int price, int miles)
+  public string Message;
+  public Car(string makeModel, int price, int miles, string message)
   {
     MakeModel = makeModel;
     Price = price;
     Miles = miles;
+    Message = message;
   }
+  public bool WorthyMiles(int maxMiles)
+  {
+    return (Miles < maxMiles);
+  }
+
   public bool WorthyPurchase(int maxPrice)
   {
     return (Price < maxPrice);
@@ -25,10 +32,10 @@ public class Program
   {
     Console.ForegroundColor = ConsoleColor.White;
     Console.BackgroundColor = ConsoleColor.Black;
-    Car bmw = new Car("1990 BMW Alpine", 110100, 4000);
-    Car chevy = new Car("2019 Chevy Silverado", 200000, 130000);
-    Car magiccarpet = new Car("2022 Tesla Magic Carpet", 210000, 100000);
-    Car magicschoolbus = new Car("1991 Cheese Bus", 1, 90000000);
+    Car bmw = new Car("1990 BMW Alpine", 110100, 4000, "Rides smooth, huge dents on sides.");
+    Car chevy = new Car("2019 Chevy Silverado", 200000, 130000, "Brakes are bad.");
+    Car magiccarpet = new Car("2022 Tesla Magic Carpet", 210000, 100000, "Needs weekly shampoo.");
+    Car magicschoolbus = new Car("1991 Cheese Bus", 1, 90000000, "Snacks and crumbs in seats.");
 
 
 
@@ -46,18 +53,37 @@ public class Program
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
 
+    Console.WriteLine("Enter maximum miles: ");
+    string stringMaxMiles = Console.ReadLine();
+    int maxMiles = int.Parse(stringMaxMiles);
+
+
     List<Car> CarsMatchingSearch = new List<Car>(0);
     foreach(Car automobile in Cars)
     {
       if (automobile.WorthyPurchase(maxPrice))
       {
+
+       if (automobile.WorthyMiles(maxMiles))
+      {
         CarsMatchingSearch.Add(automobile);
       }
+     }
+     if (!automobile.WorthyPurchase(maxPrice))
+     {
+       Console.WriteLine("Price is too low!");
+     }
     }
+
+
     foreach(Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.MakeModel);
+      Console.WriteLine(automobile.Message);
     }
-
+    if (CarsMatchingSearch.Count == 0)
+    {
+     Console.WriteLine("No matches!");
+   }
   }
 }
