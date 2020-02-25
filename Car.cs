c3fc7fc766a387eacb4ad7cc389100346e5d2dd8
Student <example@example.com>
2020-02-25 06:49:57 -0800

    add console color code

--- Car.cs
+++ Car.cs
@@ -7,12 +7,12 @@ public class Car
   public string MakeModel;
   public int Price;
   public int Miles;
-public Car(string makeModel, int price, int miles)
-{
-  MakeModel = makeModel;
-  Price = price;
-  Miles = miles;
-}
+  public Car(string makeModel, int price, int miles)
+  {
+    MakeModel = makeModel;
+    Price = price;
+    Miles = miles;
+  }
   public bool WorthyPurchase(int maxPrice)
   {
     return (Price < maxPrice);
@@ -23,10 +23,15 @@ public class Program
 {
   public static void Main()
   {
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.Black;
     Car bmw = new Car("1990 BMW Alpine", 110100, 4000);
     Car chevy = new Car("2019 Chevy Silverado", 200000, 130000);
-    Car magiccarpet = new Car("2022 Tesla Throwrug", 210000, 100000);
+    Car magiccarpet = new Car("2022 Tesla Magic Carpet", 210000, 100000);
     Car magicschoolbus = new Car("1991 Cheese Bus", 1, 90000000);
+
+
+
 
 
 
