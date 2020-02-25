7d0e53dd5c56a709c7457120ca0a92f982148133
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-24 23:02:30 -0800

    add initial wesellcars code

--- Car.cs
+++ Car.cs
@@ -1,20 +1,61 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace Car
-{
-  public class Car
-  {
-    public string MakeModel;
-    public int Price;
-    public int Miles;
-  }
 
-public class Program 
+public class Car
 {
-  public static void main()
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+
+  public bool WorthyPurchase(int maxPrice)
   {
-    
+    return (Price < maxPrice);
   }
 }
+
+public class Program
+{
+  public static void Main()
+  {
+    Car bmw = new Car();
+    bmw.MakeModel = "1990 BMW Alpine";
+    bmw.Price = 110100;
+    bmw.Miles = 4000;
+
+    Car chevy = new Car();
+    chevy.MakeModel = "2019 Chevy Silverado";
+    chevy.Price = 200000;
+    chevy.Miles = 130000;
+
+    Car magiccarpet = new Car();
+    magiccarpet.MakeModel = "2022 Tesla Throwrug";
+    magiccarpet.Price = 210000;
+    magiccarpet.Miles = 100000;
+
+    Car magicschoolbus = new Car();
+    magicschoolbus.MakeModel = "1991 Cheese Bus";
+    magicschoolbus.Price = 1;
+    magicschoolbus.Miles = 90000000;
+
+    List<Car> Cars = new List<Car>() { bmw, chevy, magiccarpet, magicschoolbus};
+
+    Console.WriteLine("Enter maximum price: ");
+    string stringMaxPrice = Console.ReadLine();
+    int maxPrice = int.Parse(stringMaxPrice);
+
+    List<Car> CarsMatchingSearch = new List<Car>(0);
+    foreach(Car automobile in Cars)
+    {
+      if (automobile.WorthyPurchase(maxPrice))
+      {
+        CarsMatchingSearch.Add(automobile);
+      }
+    }
+    foreach(Car automobile in CarsMatchingSearch)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
+
+  }
 }
